6548f3cc56f372aa17ce5118b8c63af54353a499
Eyob2022 <eyobsisay20222@gmail.com>
2022-06-30 16:58:20 -0700

    aC program that prints text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 /*
- *  * File: 4-puts.c
- *   * Auth: Eyob SIsay Tasew 
- *   
+ *File: 4-puts.c
+ *Auth: Eyob SIsay Tasew 
 */
 
 #include <stdio.h>
 
 /**
- *  * main - Prints "Programming is like building a multilingual
- *                   puzzle, followed by a new line.
+ *main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
  *
  * Return: Always 0.
- *     
+ *
  */
 int main(void)
 {
