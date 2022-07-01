3b23be7d57bb7ac6605d8f2bd59b856d6c6c02a4
Eyob2022 <eyobsisay20222@gmail.com>
2022-06-30 17:05:51 -0700

    print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 /*
  *File: 4-puts.c
- *Auth: Eyob SIsay Tasew 
-*/
+ * Auth: Eyob SIsay Tasew
+ */
 
 #include <stdio.h>
 
 /**
- *main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ *main - Prints "Programming is like building a multilingual
+ * puzzle,followed by a new line
  *
  * Return: Always 0.
  *
