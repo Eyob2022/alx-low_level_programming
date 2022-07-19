6a1d0c75674608e459787c72ed330473f9cfbcd8
Eyob2022 <eyobsisay20222@gmail.com>
2022-07-19 12:03:52 -0700

    a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,11 @@
 #define MAIN_H
 
 /**
-* File: main.h
-*/
+ * File: main.h
+ * Auth: Eyob sisay
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
 
 #include <stddef.h>
 
